685cc40bda2a09751e2590c81d8bd68f12c58722
Tom van Dijk <18gatenmaker6@gmail.com>
2025-07-12 15:55:47 +0200

    fix: welcomeText should be valid markdown

--- flake.nix
+++ flake.nix
@@ -7,15 +7,16 @@
           path = ./hm;
           description = "For starting a nice home-manager config integrated with nixvim";
           welcomeText = ''
-            First steps:
-              1. git init; git add **/*.nix; git commit -m 'initial commit'
-              2. open `flake.nix` and edit the lines with "CHANGE ME"
-              3. nix run .#activate
-              4. (optional) uncomment the nixvim stuff in flake.nix and home.nix to start configuring neovim
+          # First steps
+          1. git init; git add **/*.nix; git commit -m 'initial commit'
+          2. open `flake.nix` and edit the lines with "CHANGE ME"
+          3. nix run .#activate
+          4. (optional) uncomment the nixvim stuff in flake.nix and home.nix to start configuring neovim
 
-            How do I rebuild my config?
-              -- I've opted to not include the official `home-manager` script, and rather use `nix-community/nh`.
-              `nh home switch` should do the job.
+          ## How do I rebuild my config?
+          I've opted to not include the official `home-manager` script, and rather use `nix-community/nh`.
+          
+          `nh home switch` should do the job.
           '';
         };
 
@@ -23,10 +24,10 @@
           path = ./dev;
           description = "devshell for any language or environment";
           welcomeText = ''
-            First steps:
-              1. git init; git add flake.nix; git commit -m 'initial commit'
-              2. (if you use direnv) ln -s .envrc.recommended .envrc && direnv allow
-              3. (if you don't use direnv) nix develop
+          # First steps
+          1. git init; git add flake.nix; git commit -m 'initial commit'
+          2. (if you use direnv) ln -s .envrc.recommended .envrc && direnv allow
+          3. (if you don't use direnv) nix develop
 
           '';
         };
